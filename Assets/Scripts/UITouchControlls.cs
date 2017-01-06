1c6132e9290e70ac0e6ddfb6aa4b3d67382c65bd
thormon <thormon123@yahoo.de>
2017-01-06 18:33:56 +0100

    More UI Buttons
    Left site buttons, plus in the middle, Arrows on the extended bar, right button working

--- Assets/Scripts/UITouchControlls.cs
+++ Assets/Scripts/UITouchControlls.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 
 public class UITouchControlls : MonoBehaviour {
 
+	public Text announcer;
+
+	public AudioSource fightaudio;
+
+	public Animator topinfo;
 	public Animator actionButton;
-	public Animator sidebar;
+	public Animator charsheet;
 	public Animator actionPlus;
-	public GameObject movebutton;
+
+	public GameObject char_button_1;
+	public GameObject char_button_2;
+	public GameObject char_button_3;
 
 	private bool animationState = false;
+	private int rounds;
 	// Use this for initialization
 	void Start () {
-		movebutton.SetActive(false);
+		char_button_1.SetActive (false);
+		char_button_2.SetActive (false);
+		char_button_3.SetActive (false);
+
 
 	}
 	
@@ -25,40 +37,97 @@ public class UITouchControlls : MonoBehaviour {
 
 
 
-	public void SwitchSideBar (){
+	public void SwitchCharSheet (){
+		animationState = charsheet.GetBool ("isopen");
+		if (animationState == false) {
+			charsheet.SetBool ("isopen", true);
+
+		} else {
+			charsheet.SetBool ("isopen", false);
+
+
+		}
+	}
+
+	public void SwitchActionBar (){
 		animationState = actionButton.GetBool ("actionbar_open");
 		if (animationState == false) {
-			movebutton.SetActive (true);
 			actionButton.SetBool ("actionbar_open", true);
 			actionPlus.SetBool ("action_closed", true);
 			//Debug.Log ("open");
 		} else {
 			actionButton.SetBool ("actionbar_open", false);
 			actionPlus.SetBool ("action_closed", false);
-			movebutton.SetActive (false);
-
-			//Debug.Log ("close");
-
-		}
-	}
-
-	public void SwitchActionButton (){
-		animationState = actionButton.GetBool("actionbar_open");
-		if (animationState == false) {
-			movebutton.SetActive(true);
-			actionButton.SetBool ("actionbar_open", true);
-			actionPlus.SetBool ("action_closed", true);
-			//Debug.Log ("open");
-		}
-		else {
-		actionButton.SetBool ("actionbar_open", false);
-			actionPlus.SetBool ("action_closed", false);
-			movebutton.SetActive(false);
 
 			//Debug.Log ("close");
 		
 		}
-	
-	
 	}
-}
+
+	public void RoundManager (){
+		rounds++;
+		switch (rounds){
+
+		case 1:
+			announcer.text = "Draw your card";
+			topinfo.SetBool ("topaction_open", true);
+
+			break;
+		case 2:
+			announcer.text = "Move your unit";
+			break;
+
+		case 3:
+			announcer.text = "Fight";
+			fightaudio.Play ();
+			break;
+
+		case 4:
+			topinfo.SetBool ("topaction_open", false);
+			announcer.text = "";
+			rounds = 0;
+			break;
+
+		}
+			
+		}
+
+
+	public void ModelViewer(int buttonnumber){
+
+		switch (buttonnumber) {
+
+		case 1:
+			if (char_button_1.activeSelf == true) {
+				char_button_1.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (true);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (false);
+			break;
+
+		case 2:
+			if (char_button_2.activeSelf == true) {
+				char_button_2.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (true);
+			char_button_3.SetActive (false);
+			break;
+
+		case 3:
+			if (char_button_3.activeSelf == true) {
+				char_button_3.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (true);
+			break;
+		
+		
+		}
+		}
+		}
