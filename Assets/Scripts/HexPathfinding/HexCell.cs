ccac5c817492039cc988a12dd99171865e5bbc2b
Illauriel <gorionsl@gmail.com>
2017-01-09 17:14:24 +0100

    Pathfinding Merge
    Throwing in pathfinding logic

---
+++ Assets/Scripts/HexPathfinding/HexCell.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexMap{
+	public struct HexCell { //it's p-toointy tpped
+
+		public readonly int q; // upper right axis or cubic "x" or column
+		public readonly int r; // down axis or cubic "z" or row
+		public readonly int s; // down right axis??
+		//public readonly int h;// cell height over "ground"
+
+		public HexCell(int q, int r, int s){
+			this.q = q;
+			this.r = r;
+			this.s = s;
+		}
+		public HexCell(int q, int r){
+			this.q = q;
+			this.r = r;
+			this.s = -q - r;
+		}
+
+		static int HexLength(HexCell hex){
+			return (int)((Mathf.Abs(hex.q)+Mathf.Abs(hex.r)+Mathf.Abs(hex.s))/2);
+		}
+
+		public static int Distance(HexCell a, HexCell b){
+			return HexLength(a-b);
+		}
+
+		HexCell Direction (int dir){
+			return directions[dir];
+		}
+
+		/*public static HexCell GetNeighbor(HexCell hex, int dir){
+			return hex + directions[dir];
+		}*/
+
+		public HexCell Neighbor(int dir){
+			return this + directions[dir];
+		}
+
+
+
+
+		// +, -, ==, !=
+		public static HexCell operator + (HexCell a, HexCell b){ 
+			return new HexCell(a.q+b.q, a.r+b.r, a.s+b.s); 
+		}
+		public static HexCell operator - (HexCell a, HexCell b){ 
+			return new HexCell(a.q-b.q, a.r-b.r, a.s-b.s); 
+		}
+		public static HexCell operator - (HexCell a, int k){ 
+			return new HexCell(a.q*k, a.r*k, a.s*k); 
+		}
+		public static bool operator == (HexCell a, HexCell b){
+			return a.q == b.q && a.r == b.r && a.s == b.s;
+		}
+		public static bool operator != (HexCell a, HexCell b){
+			return !(a == b);
+		}
+		public override bool Equals(object o){ 
+			return (o is HexCell) && this == (HexCell)o; 
+		}
+		public override int GetHashCode(){
+			return q & (int)0xFFFF | r<<16; // I have no idea what's happening here (>囗＜?)
+		}
+
+		static readonly HexCell[] directions = {
+
+			new HexCell(0, 1, -1), 
+			new HexCell(1, 0, -1),
+			new HexCell(1, -1, 0),
+			new HexCell(0, -1, 1),
+			new HexCell(-1, 0, 1),
+			new HexCell(-1, 1, 0)
+		};
+	}
+
+
+	struct FractionalHex
+	{
+	    public FractionalHex(double q, double r, double s)
+	    {
+	        this.q = q;
+	        this.r = r;
+	        this.s = s;
+	    }
+	    public readonly double q;
+	    public readonly double r;
+	    public readonly double s;
+
+	    static public HexCell HexRound(FractionalHex h)
+	    {
+	        int q = (int)(Math.Round(h.q));
+	        int r = (int)(Math.Round(h.r));
+	        int s = (int)(Math.Round(h.s));
+	        double q_diff = Math.Abs(q - h.q);
+	        double r_diff = Math.Abs(r - h.r);
+	        double s_diff = Math.Abs(s - h.s);
+	        if (q_diff > r_diff && q_diff > s_diff)
+	        {
+	            q = -r - s;
+	        }
+	        else
+	            if (r_diff > s_diff)
+	            {
+	                r = -q - s;
+	            }
+	            else
+	            {
+	                s = -q - r;
+	            }
+	        return new HexCell(q, r, s);
+	    }
+
+
+	    static public FractionalHex HexLerp(FractionalHex a, FractionalHex b, double t)
+	    {
+	        return new FractionalHex(a.q * (1 - t) + b.q * t, a.r * (1 - t) + b.r * t, a.s * (1 - t) + b.s * t);
+	    }
+
+
+	    static public List<HexCell> HexLinedraw(HexCell a, HexCell b)
+	    {
+	        int N = HexCell.Distance(a, b);
+	        FractionalHex a_nudge = new FractionalHex(a.q + 0.000001, a.r + 0.000001, a.s - 0.000002);
+	        FractionalHex b_nudge = new FractionalHex(b.q + 0.000001, b.r + 0.000001, b.s - 0.000002);
+	        List<HexCell> results = new List<HexCell>{};
+	        double step = 1.0 / Math.Max(N, 1);
+	        for (int i = 0; i <= N; i++)
+	        {
+	            results.Add(FractionalHex.HexRound(FractionalHex.HexLerp(a_nudge, b_nudge, step * i)));
+	        }
+	        return results;
+	    }
+
+	}
+}
---
+++ Assets/Scripts/HexPathfinding/MeshGen.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+
+namespace HexMap{
+	public class MeshGen {
+		//static float SQRT3 = Mathf.Sqrt(3);
+
+		public static Mesh Square(float extent){
+			Mesh result = new Mesh();
+
+			Vector3[] vertices = new Vector3[]
+			{
+				new Vector3( extent, 0,  extent),
+				new Vector3( extent, 0, -extent),
+				new Vector3(-extent, 0,  extent),
+				new Vector3(-extent, 0, -extent)
+			};
+
+			Vector2[] uv = new Vector2[]
+			{
+				new Vector2(1, 1),
+				new Vector2(1, 0),
+				new Vector2(0, 1),
+				new Vector2(0, 0)
+			};
+
+			int[] triangles = new int[]
+			{
+				0, 1, 2,
+				2, 1, 3
+			};
+
+			result.vertices = vertices;
+			result.uv = uv;
+			result.triangles = triangles;
+
+			return result;
+		}
+
+		public static Mesh Hex(float radius){
+			Mesh result = new Mesh();
+			//float r = extent/(SQRT3/2f);
+
+			Vector3[] p = new Vector3[7];
+			p[0] = Vector3.zero;
+			for (int i = 1; i <= 6; i++) {
+				p[i] = new Vector3(radius*Mathf.Cos(2*Mathf.PI*i/6), 0, radius*Mathf.Sin(2*Mathf.PI*i/6));
+			}
+
+			   /*[] // point array
+			{
+				new Vector3 (0,0,0),
+				new Vector3 (r*Mathf.Cos(2*Mathf.PI*1/6), 0, r*Mathf.Sin(2*Mathf.PI*1/6)),
+				new Vector3 (r*Mathf.Cos(2*Mathf.PI*2/6), 0, r*Mathf.Sin(2*Mathf.PI*2/6)),
+				new Vector3 (r*Mathf.Cos(2*Mathf.PI*3/6), 0, r*Mathf.Sin(2*Mathf.PI*3/6)),
+				new Vector3 (r*Mathf.Cos(2*Mathf.PI*4/6), 0, r*Mathf.Sin(2*Mathf.PI*4/6)),
+				new Vector3 (r*Mathf.Cos(2*Mathf.PI*5/6), 0, r*Mathf.Sin(2*Mathf.PI*5/6)),
+				new Vector3 (r*Mathf.Cos(2*Mathf.PI*6/6), 0, r*Mathf.Sin(2*Mathf.PI*6/6))
+			};*/
+			
+
+			Vector3[] vertices = new Vector3[]{
+				//p1,p2,p0,
+				p[0],p[2],p[1], // 0,2,1,4,7,10,13
+				p[0],p[3],p[2],
+				p[0],p[4],p[3],
+				p[0],p[5],p[4],
+				p[0],p[6],p[5],
+				p[0],p[1],p[6]
+			};
+			int[] triangles = new int[]
+			{
+			    0,1,2,
+			    3,4,5,
+			    6,7,8,
+			    9,10,11,
+				12,13,14,
+				15,16,17
+			};
+			
+			Vector2 uv0 = new Vector2(0.5f, 0f);
+			Vector2 uv1 = new Vector2(0, 1f);
+			Vector2 uv2 = new Vector2(1, 1);
+			
+			Vector2[] uv = new Vector2[]{
+	    		uv0,uv1,uv2,
+				uv0,uv1,uv2,
+				uv0,uv1,uv2,
+				uv0,uv1,uv2,
+				uv0,uv1,uv2,
+				uv0,uv1,uv2
+				
+			};
+
+			result.vertices = vertices;
+			result.uv = uv;
+			result.triangles = triangles;
+			
+			return result;
+		}
+
+		///Creates a combined Mesh of squares centered at coords
+		public static Mesh CreateGridMesh(Vector3[] coords){
+			Mesh mesh = MeshGen.Square(0.5f);
+			return CreateGridMesh(mesh, coords);
+		}
+
+		public static Mesh CreateGridMesh(Mesh mesh, Vector3[] coords){
+			Mesh result = new Mesh();
+			CombineInstance[] combineDetails = new CombineInstance[coords.Length];
+			for (int i = 0; i < coords.Length; i++) {
+				Matrix4x4 translation = TranslationMatrix(coords[i]);
+
+				combineDetails[i].mesh = mesh;
+				combineDetails[i].transform = translation;
+			}
+			result.CombineMeshes(combineDetails);
+			return result;
+		}
+
+		private static Matrix4x4 TranslationMatrix(Vector3 offset) {
+			Matrix4x4 matrix = Matrix4x4.identity;
+			matrix.m03 = offset.x;
+			matrix.m13 = offset.y;
+			matrix.m23 = offset.z;
+			return matrix;
+		}
+	}
+}
