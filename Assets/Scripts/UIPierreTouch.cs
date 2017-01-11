0aa5096d57af4ea9b9d551e9aeb5443fa9b9ce24
thormon <thormon123@yahoo.de>
2017-01-11 02:48:42 +0100

    8 model audience apk
    more models, new ar database with johnnys image as target

--- Assets/Scripts/UIPierreTouch.cs
+++ Assets/Scripts/UIPierreTouch.cs
@@ -17,6 +17,11 @@ public class UIPierreTouch: MonoBehaviour {
 	public GameObject char_button_1;
 	public GameObject char_button_2;
 	public GameObject char_button_3;
+	public GameObject char_button_4;
+	public GameObject char_button_5;
+	public GameObject char_button_6;
+	public GameObject char_button_7;
+	public GameObject char_button_8;
 
 	//	public GameObject rotate_button_left;
 	//	public GameObject rotate_button_right;
@@ -34,6 +39,13 @@ public class UIPierreTouch: MonoBehaviour {
 	private Quaternion char_1_rotation;
 	private Quaternion char_2_rotation;
 	private Quaternion char_3_rotation;
+	private Quaternion char_4_rotation;
+	private Quaternion char_5_rotation;
+	private Quaternion char_6_rotation;
+	private Quaternion char_7_rotation;
+	private Quaternion char_8_rotation;
+
+
 
 	private GameObject toberotated;
 
@@ -42,6 +54,12 @@ public class UIPierreTouch: MonoBehaviour {
 		char_button_1.SetActive (false);
 		char_button_2.SetActive (false);
 		char_button_3.SetActive (false);
+		char_button_4.SetActive (false);
+		char_button_5.SetActive (false);
+		char_button_6.SetActive (false);
+		char_button_7.SetActive (false);
+		char_button_8.SetActive (false);
+
 		rotate_slider.SetActive(false);
 		freeze_button.SetActive (false);
 		unfreeze_button.SetActive (false);
@@ -50,6 +68,13 @@ public class UIPierreTouch: MonoBehaviour {
 		char_1_rotation = char_button_1.transform.rotation;
 		char_2_rotation = char_button_2.transform.rotation;
 		char_3_rotation = char_button_3.transform.rotation;
+		char_4_rotation = char_button_4.transform.rotation;
+		char_5_rotation = char_button_5.transform.rotation;
+		char_6_rotation = char_button_6.transform.rotation;
+		char_7_rotation = char_button_7.transform.rotation;
+		char_8_rotation = char_button_8.transform.rotation;
+
+
 	}
 
 	// Update is called once per frame
@@ -135,6 +160,12 @@ public class UIPierreTouch: MonoBehaviour {
 			char_button_1.SetActive (true);
 			char_button_2.SetActive (false);
 			char_button_3.SetActive (false);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (false);
+
 			toberotated = char_button_1;
 			rotate_slider.SetActive (true);
 			slider.value = 0f;
@@ -155,6 +186,13 @@ public class UIPierreTouch: MonoBehaviour {
 			char_button_1.SetActive (false);
 			char_button_2.SetActive (true);
 			char_button_3.SetActive (false);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (false);
+
+
 			toberotated = char_button_2;
 			rotate_slider.SetActive(true);
 			slider.value = 0f;
@@ -175,14 +213,140 @@ public class UIPierreTouch: MonoBehaviour {
 			char_button_1.SetActive (false);
 			char_button_2.SetActive (false);
 			char_button_3.SetActive (true);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (false);
+
+
 			toberotated = char_button_3;
 			rotate_slider.SetActive(true);
 			slider.value = 0f;
 			char_button_3.transform.rotation = char_3_rotation;
 			freeze_button.SetActive (true);
-
 			break;
+//.----------
+		case 4:
+			if (char_button_4.activeSelf == true) {
+				char_button_4.SetActive (false);
+				rotate_slider.SetActive(false);
+				freeze_button.SetActive (false);
+				unfreeze_button.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (false);
+			char_button_4.SetActive (true);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (false);
 
+
+			toberotated = char_button_4;
+			rotate_slider.SetActive(true);
+			slider.value = 0f;
+			char_button_4.transform.rotation = char_4_rotation;
+			freeze_button.SetActive (true);
+			break;
+		case 5:
+			if (char_button_5.activeSelf == true) {
+				char_button_5.SetActive (false);
+				rotate_slider.SetActive(false);
+				freeze_button.SetActive (false);
+				unfreeze_button.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (false);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (true);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (false);
+
+
+			toberotated = char_button_5;
+			rotate_slider.SetActive(true);
+			slider.value = 0f;
+			char_button_5.transform.rotation = char_5_rotation;
+			freeze_button.SetActive (true);
+			break;
+		case 6:
+			if (char_button_6.activeSelf == true) {
+				char_button_6.SetActive (false);
+				rotate_slider.SetActive(false);
+				freeze_button.SetActive (false);
+				unfreeze_button.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (false);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (true);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (false);
+
+
+			toberotated = char_button_6;
+			rotate_slider.SetActive(true);
+			slider.value = 0f;
+			char_button_6.transform.rotation = char_6_rotation;
+			freeze_button.SetActive (true);
+			break;
+		case 7:
+			if (char_button_7.activeSelf == true) {
+				char_button_7.SetActive (false);
+				rotate_slider.SetActive(false);
+				freeze_button.SetActive (false);
+				unfreeze_button.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (false);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (true);
+			char_button_8.SetActive (false);
+
+
+			toberotated = char_button_7;
+			rotate_slider.SetActive(true);
+			slider.value = 0f;
+			char_button_7.transform.rotation = char_7_rotation;
+			freeze_button.SetActive (true);
+			break;
+		case 8:
+			if (char_button_8.activeSelf == true) {
+				char_button_8.SetActive (false);
+				rotate_slider.SetActive(false);
+				freeze_button.SetActive (false);
+				unfreeze_button.SetActive (false);
+				break;
+			}
+			char_button_1.SetActive (false);
+			char_button_2.SetActive (false);
+			char_button_3.SetActive (false);
+			char_button_4.SetActive (false);
+			char_button_5.SetActive (false);
+			char_button_6.SetActive (false);
+			char_button_7.SetActive (false);
+			char_button_8.SetActive (true);
+
+
+			toberotated = char_button_8;
+			rotate_slider.SetActive(true);
+			slider.value = 0f;
+			char_button_8.transform.rotation = char_8_rotation;
+			freeze_button.SetActive (true);
+			break;
 
 		}
 	}
