b2ef4880ce45f0f2fed63bb30e8ddf24e6b398bd
DanielFM <daniFM333@gmail.com>
2017-01-14 16:31:34 +0100

    Animation support
    All scene changes made in "TestAnimations" scene.
    Created animator controller for the units, with placeholder animations.
    Movement speed of units needs to be fixed.

--- Assets/Scripts/HexFauxTest/FireWeapon.cs
+++ Assets/Scripts/HexFauxTest/FireWeapon.cs
@@ -6,13 +6,16 @@ public class FireWeapon : MonoBehaviour {
 
 	public Transform hardpoint;
 	public GameObject particle;
+    public Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        anim.GetComponent<Animator>();
+    }
 	
-	public void Shoot(){
-		GameObject prt = (GameObject) Instantiate(particle, hardpoint.position, hardpoint.rotation);
+	public void Shoot() {
+        anim.Play("Fire");
+        GameObject prt = (GameObject) Instantiate(particle, hardpoint.position, hardpoint.rotation);
 		Destroy(prt, 3);
 	}
 }
