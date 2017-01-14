0ee4f69fc0c3f9833a846cc516b680916df3a0ac
alexorbh7 <alexorbh7@hotmail.com>
2017-01-14 17:23:52 +0100

    CardScanning
    Added UI system of applying a Card.
    Needs:
    - communication with the card system.
    - a solution to highlight the portrait: by programming or with a new texture.

---
+++ Assets/ScannedCardActivator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script will be inside the image target.
+/// Will be activated when the imagetarget is scanned
+/// This will activate the control of injection cards
+/// </summary>
+public class ScannedCardActivator : MonoBehaviour {
+
+    public GameObject activateScannedCard;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //activate the AttributeCardInjector or EffectCardInjector
+
+        //activateScannedCard = GameObject.Find("");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    
+
+
+
+
+}
